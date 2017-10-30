df51f0cad368f8e3d3d117d1abf55578be9002d7
mmicciolo <mmicciolo@wpi.edu>
2017-10-30 19:14:43 -0400

    New Project. New FPS controller.

---
+++ private/source/circuit-vr/Assets/Source/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Source
+{
+    [InitializeOnLoad]
+    class Startup
+    {
+        static Startup()
+        {
+            Debug.Log("Up and running");
+        }
+    }
+}
