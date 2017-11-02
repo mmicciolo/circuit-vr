552d17a79dd38e3e0e6043ce4d49078b0285097b
Long Vu <txvu@wpi.edu>
2017-11-02 22:38:59 +0700

    Main menu scene created - leads to spaceship scene - auto sliding door prefab working

---
+++ private/source/circuit-vr/Assets/AutoDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDoor : MonoBehaviour {
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("entered");
+        //animation.Play("DoorOpen");
+        gameObject.GetComponent<Animator>().Play("DoorOpen");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exited");
+        //animator.Play("DoorClose");
+        gameObject.GetComponent<Animator>().Play("DoorClose");
+    }
+
+}
---
+++ private/source/circuit-vr/Assets/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject SettingsCanvas;
+
+	// Use this for initialization
+	void Start () {
+        SettingsCanvas.SetActive(false);
+	}
+	
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SpaceShip Scaled");
+    }
+
+    public void SettingsButtonClicked()
+    {
+        SettingsCanvas.SetActive(!SettingsCanvas.active);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
