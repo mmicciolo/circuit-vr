13b0c278c27e9250fbdda3037e785e822e1d60da
Long Vu <txvu@wpi.edu>
2017-11-07 03:26:38 +0700

    Door animations fixed, not optimized

--- private/source/circuit-vr/Assets/Source/Door/AutoDoor.cs
+++ private/source/circuit-vr/Assets/Source/Door/AutoDoor.cs
@@ -6,28 +6,59 @@ public class AutoDoor : MonoBehaviour {
 
     public Animator animator;
 
+    bool needsOpen;
+    bool isOpen;
+    bool canPlayAnimation;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
+        animator = gameObject.GetComponent<Animator>();
+        isOpen = false;
+        needsOpen = false;
+        canPlayAnimation = true;
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if ((isOpen == true) && (canPlayAnimation == true))
+        {
+            canPlayAnimation = false;
+            animator.Play("DoorOpen");
+            Debug.Log("door opening");
+            StartCoroutine(WaitForDoorAnimation());
+        }
+        if ((isOpen == false) && (canPlayAnimation == true))
+        {
+            canPlayAnimation = false;
+            animator.Play("DoorClose");
+            Debug.Log("door closing");
+            StartCoroutine(WaitForDoorAnimation());
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("entered");
-        //animation.Play("DoorOpen");
-        gameObject.GetComponent<Animator>().Play("DoorOpen");
+
+        isOpen = true;
+
     }
 
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("exited");
-        //animator.Play("DoorClose");
-        gameObject.GetComponent<Animator>().Play("DoorClose");
+
+        isOpen = false;
     }
 
+    IEnumerator WaitForDoorAnimation()
+    {
+        while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+        {
+            yield return null;
+        }
+        Debug.Log("Coroutine created");
+        canPlayAnimation = true;
+
+    }
 }
