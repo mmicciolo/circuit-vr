bf42ba3982beec92633caf581ee0b68645719f66
mmicciolo <mmicciolo@wpi.edu>
2017-11-07 17:05:48 -0500

    Interactive Puzzles + base puzzle

---
+++ private/source/circuit-vr/Assets/Source/Puzzles/Puzzle.cs
@@ -0,0 +1,12 @@
+﻿using Assets.Source.Interactable_Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Source.Puzzles
+{
+    class Puzzle : InteractablePuzzle
+    {
+    }
+}
