29965f5058a0794ae60d6828c227f79edeb2e133
Long Vu <txvu@wpi.edu>
2017-11-20 09:23:52 +0700

    draggable components in puzzle K1 and K2

---
+++ private/source/circuit-vr/Assets/Source/Puzzles/Components/DraggableCircuitComponent.cs
@@ -0,0 +1,60 @@
+﻿using Assets.Source.Puzzles.Grids;
+using Assets.Source.Puzzles.Grids.Cells;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Source.Puzzles.Components
+{
+    class DraggableCircuitComponent : CircuitComponent
+    {
+        public bool moved = false;
+        public Vector2 initialPos;
+        Puzzle currentPuzzle;
+
+        private void Start()
+        {
+            initialPos = componentPosition;
+            currentPuzzle = GameObject.FindObjectOfType<Puzzle>();
+        }
+
+        private void OnMouseDrag()
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObject.transform.position.z);
+
+            //Based off of the above point calculate a transform
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+
+            //Set the transform to the dragging object
+            //Lock the Z position
+            gameObject.transform.position = new Vector3(curPosition.x, curPosition.y, gameObject.transform.position.z);
+        }
+
+        private void Update()
+        {
+        }
+
+        private void OnMouseUp()
+        {
+            float toAnswer = Vector2.Distance(gameObject.transform.position, PuzzleGrid.GetPuzzleGrid().getCell(currentPuzzle.outputPosition.componentPosition).transform.position);
+            float toIni = Vector2.Distance(gameObject.transform.position, PuzzleGrid.GetPuzzleGrid().getCell(initialPos).transform.position);
+            if (toIni < toAnswer)
+            {
+                moved = false;
+                setComponentToCell(initialPos);
+            }
+            else
+            {
+                currentPuzzle.ResetChoices();
+                moved = true;
+
+                Vector2 newPos = currentPuzzle.outputPosition.componentPosition;
+                setComponentToCell(newPos);
+            }
+        }
+
+
+    }
+}
