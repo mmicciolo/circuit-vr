9aca06f35c012516d0ce8bd71a1fcf9e026e28ab
Long Vu <txvu@wpi.edu>
2017-11-20 08:24:42 +0700

    Puzzle K2 working + UI scenes imported

---
+++ private/source/circuit-vr/Assets/Source/Puzzles/PuzzleK2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Source.Puzzles.Components;
+using UnityEngine;
+
+namespace Assets.Source.Puzzles
+{
+    class PuzzleK2 : Puzzle
+    {
+        public ClickableCircuitComponent[] choices;
+
+        private void Start()
+        {
+            //ActivateCells(0);
+        }
+
+        private void Update()
+        {
+            if ((choices[2].componentPosition.x == outputPosition.componentPosition.x) && (choices[2].componentPosition.y == outputPosition.componentPosition.y))
+            {
+                Debug.Log("Puzzle solved");
+            }
+        }
+
+        override
+        public void ResetChoices()
+        {
+            for (int i = 0; i < choices.Length; i++)
+            {
+                choices[i].moved = false;
+                choices[i].setComponentToCell(choices[i].initialPos);
+            }
+        }
+    }
+}
