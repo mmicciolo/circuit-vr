6d2ed1e03c21e30a1a3836af75856cad15eeeb7c
mmicciolo <mmicciolo@wpi.edu>
2017-11-30 01:38:29 -0500

    SPST switch and push button prefabs.

---
+++ private/source/circuit-vr/Assets/Source/Puzzles/Components/PushButtonComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Source.Puzzles.Components
+{
+    class PushButtonComponent : CircuitComponent
+    {
+        private Animator animator;
+
+        public void Start()
+        {
+            animator = gameObject.GetComponentsInChildren<Animator>()[0];
+            GetOriginalMaterial();
+        }
+
+        public void OnMouseDown()
+        {
+            animator.Play("button_down");
+        }
+
+        public void OnMouseUp()
+        {
+            animator.Play("button_up");
+        }
+    }
+}
---
+++ private/source/circuit-vr/Assets/Source/Puzzles/Components/SPSTSwitchCircuitComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Source.Puzzles.Components
+{
+    class SPSTSwitchCircuitComponent : CircuitComponent
+    {
+        private Animator animator;
+        private AudioSource audio;
+        public String lastAnimation = "switch_idle";
+
+        public void Start()
+        {
+            animator = gameObject.GetComponentsInChildren<Animator>()[0];
+            audio = gameObject.GetComponentsInChildren<AudioSource>()[0];
+            GetOriginalMaterial();
+        }
+
+        public void OnMouseDown()
+        {
+            audio.Play();
+            if (lastAnimation.Equals("switch_idle") || lastAnimation.Equals("spst_up"))
+            {
+                animator.Play("spst_down");
+                lastAnimation = "spst_down";
+            }
+            else if (lastAnimation.Equals("spst_down"))
+            {
+                animator.Play("spst_up");
+                lastAnimation = "spst_up";
+            }
+        }
+    }
+}
