6047c6e53c32d1cc0516b7f46441e5c851e74378
mmicciolo <mmicciolo@wpi.edu>
2017-11-29 21:52:07 -0500

    Grid Hiding / Showing.

--- private/source/circuit-vr/Assets/Source/Puzzles/Grids/PuzzleGrid.cs
+++ private/source/circuit-vr/Assets/Source/Puzzles/Grids/PuzzleGrid.cs
@@ -16,6 +16,8 @@ namespace Assets.Source.Puzzles.Grids
         public Vector2 gridSize;
         public Vector2 cellSize;
 
+        private bool gridHidden = false;
+
         private void Start()
         {
             
@@ -39,6 +41,20 @@ namespace Assets.Source.Puzzles.Grids
                         cc.InitComponent();
                     }
                 }
+                AddGridCollider();
+                //HideGrid();
+                StartCoroutine(ShowGridInitially());
+            }
+            CheckForMouseOver();
+        }
+
+        private System.Collections.IEnumerator ShowGridInitially()
+        {
+            while(true)
+            {
+                yield return new WaitForSeconds(2);
+                HideGrid();
+                gridHidden = true;
             }
         }
 
@@ -57,6 +73,66 @@ namespace Assets.Source.Puzzles.Grids
                     cellGameObject.transform.parent = gameObject.transform;
                     cellObjects.Add(cellGameObject);
                     puzzleCells.Add(cell);
+                }
+            }
+        }
+
+        //We cant use OnMouseEnter or OnMouseExit because of other colliders in the scene
+        //Therefore we must use a raycast all and see if one of the objects is this puzzle grid
+        public void CheckForMouseOver()
+        {
+            RaycastHit[] hits;
+            Ray ray = GameObject.Find("Main Camera").GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            hits = Physics.RaycastAll(ray);
+            if (hits.Length > 0)
+            {
+                foreach(RaycastHit hit in hits)
+                {
+                    if(hit.transform.name.Equals(transform.name))
+                    {
+                        if(gridHidden)
+                        {
+                            ShowGrid();
+                            gridHidden = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if(gridHidden)
+                {
+                    HideGrid();
+                }
+            }
+        }
+
+        private void AddGridCollider()
+        {
+            BoxCollider collider = gameObject.AddComponent<BoxCollider>() as BoxCollider;
+            collider.center = new Vector3((gridSize.x / 2) * cellSize.x, ((gridSize.y / 2) * cellSize.y) - cellSize.y, transform.localPosition.z);
+            collider.size = new Vector3(gridSize.x * cellSize.x, gridSize.y * cellSize.y, 1.0f);
+        }
+
+        public void ShowGrid()
+        {
+            foreach (GameObject cell in cellObjects)
+            {
+                foreach (LineRenderer line in cell.GetComponentsInChildren<LineRenderer>())
+                {
+                    line.enabled = true;
+                }
+            }
+        }
+
+        public void HideGrid()
+        {
+            foreach(GameObject cell in cellObjects)
+            {
+                foreach(LineRenderer line in cell.GetComponentsInChildren<LineRenderer>())
+                {
+                    line.enabled = false;
                 }
             }
         }
