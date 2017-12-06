2567bc0d92683756e639de140f7cc667d93f1fe2
mmicciolo <mmicciolo@wpi.edu>
2017-12-06 16:22:12 -0500

    Added Glove. New Sounds.

--- private/source/circuit-vr/Assets/Source/Puzzles/Components/PushButtonComponentClosed.cs
+++ private/source/circuit-vr/Assets/Source/Puzzles/Components/PushButtonComponentClosed.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using FMODUnity;
 
 namespace Assets.Source.Puzzles.Components
 {
     class PushButtonComponentClosed : CircuitComponent
     {
         private Animator animator;
+        private StudioEventEmitter press;
+        private StudioEventEmitter release;
 
         public void Start()
         {
             animator = gameObject.GetComponentsInChildren<Animator>()[0];
+            press = GetComponents<StudioEventEmitter>()[0];
+            release = GetComponents<StudioEventEmitter>()[1];
             GetOriginalMaterial();
         }
 
@@ -21,6 +26,7 @@ namespace Assets.Source.Puzzles.Components
             if (enabled)
             {
                 animator.Play("push_button_closed_down");
+                release.Play();
             }
         }
 
@@ -29,6 +35,7 @@ namespace Assets.Source.Puzzles.Components
             if (enabled)
             {
                 animator.Play("push_button_closed_up");
+                press.Play();
             }
         }
     }
