1d8345a8bc38cae446197ff1b6b58068c2589e2b
Long Vu <txvu@wpi.edu>
2017-12-14 13:38:49 +0700

    Powercell activation

--- private/source/circuit-vr/Assets/Source/PowerCell.cs
+++ private/source/circuit-vr/Assets/Source/PowerCell.cs
@@ -5,17 +5,46 @@ using UnityEngine;
 public class PowerCell : MonoBehaviour {
 
     public Animator animator;
+    public bool activated;
+    public int puzzleNumber;
+    float cellGlowPower = 2.5f;
+    float sphereGlowPower = 0.97f;
+    Material cellGlowMaterial = null;
+    Material sphereGlowMaterial = null;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         animator = GetComponent<Animator>();
-	}
+        foreach (Transform t in GetComponentsInChildren<Transform>())
+        {
+            if (t.name.Equals("power_cell_low"))
+            {
+                cellGlowMaterial = t.gameObject.GetComponent<Renderer>().material;
+            }
+            if (t.name.Equals("power_sphere_hi"))
+            {
+                sphereGlowMaterial = t.gameObject.GetComponent<Renderer>().material;
+            }
+        }
+        activated = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if(enabled)
+        //activated = LevelController.getInstance().CheckDoorCanOpen(puzzleNumber);
+        activated = true;
+
+		if(activated)
         {
+            cellGlowMaterial.SetFloat("_MKGlowPower", cellGlowPower);
+            sphereGlowMaterial.SetFloat("_MKGlowPower", sphereGlowPower);
             animator.Play("powercell_outer_ring");
+        }
+        else
+        {
+            cellGlowMaterial.SetFloat("_MKGlowPower", 0);
+            sphereGlowMaterial.SetFloat("_MKGlowPower", 0);
+            animator.StopPlayback();
         }
 	}
 }
