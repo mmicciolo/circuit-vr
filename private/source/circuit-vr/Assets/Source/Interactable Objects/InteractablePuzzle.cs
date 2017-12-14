9599a5d7a13a208691ddc122745276084d892b0d
mmicciolo <mmicciolo@wpi.edu>
2017-12-13 19:54:49 -0500

    Tablet Glowing.

--- private/source/circuit-vr/Assets/Source/Interactable Objects/InteractablePuzzle.cs
+++ private/source/circuit-vr/Assets/Source/Interactable Objects/InteractablePuzzle.cs
@@ -7,16 +7,60 @@ using UnityEngine.SceneManagement;
 
 namespace Assets.Source.Interactable_Objects
 {
+    enum GlowColor
+    {
+        NONE,
+        RED,
+        GREEN,
+        BLUE
+    }
+
     class InteractablePuzzle : MonoBehaviour
     {
 		public int puzzleNumber;
         string sceneName;
+
+        private GameObject tablet = null;
+        private Material tabletMaterial = null;
+        private Material tabletGlowRed = null;
+        private Material tabletGlowGreen = null;
+        private Material tabletGlowBlue = null;
+
+        public void Start()
+        {
+            tablet = gameObject.transform.Find("pCube1").gameObject;
+            tabletMaterial = tablet.GetComponent<Renderer>().material;
+            tabletGlowRed = Resources.Load<Material>("tablet_glow_red") as Material;
+            tabletGlowGreen = Resources.Load<Material>("tablet_glow_green") as Material;
+            tabletGlowBlue = Resources.Load<Material>("tablet_glow_blue") as Material;
+        }
 
         public virtual void open()
         {
 			sceneName = LevelController.getInstance ().puzzleOrder [puzzleNumber];
 			Debug.Log (sceneName);
             LevelController.getInstance().openPuzzle(sceneName);
+        }
+
+        public void SetGlow(GlowColor color)
+        {
+            switch(color)
+            {
+                case GlowColor.NONE:
+                    tablet.GetComponent<Renderer>().material = tabletMaterial;
+                    break;
+                case GlowColor.RED:
+                    tablet.GetComponent<Renderer>().material = tabletGlowRed;
+                    break;
+                case GlowColor.GREEN:
+                    tablet.GetComponent<Renderer>().material = tabletGlowGreen;
+                    break;
+                case GlowColor.BLUE:
+                    tablet.GetComponent<Renderer>().material = tabletGlowBlue;
+                    break;
+                default:
+                    break;
+            }
         }
 
         //private void OnPuzzleLoaded(Scene scene, LoadSceneMode mode)
