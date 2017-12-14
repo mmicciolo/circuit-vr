8cf687e5a92fd3efa98337ad562e3777f1f4fdde
Long Vu <txvu@wpi.edu>
2017-12-14 09:40:36 +0700

    environmental changes

--- private/source/circuit-vr/Assets/Source/Interactable Objects/InteractablePuzzle.cs
+++ private/source/circuit-vr/Assets/Source/Interactable Objects/InteractablePuzzle.cs
@@ -63,24 +63,74 @@ namespace Assets.Source.Interactable_Objects
             }
         }
 
+        //private void OnPuzzleLoaded(Scene scene, LoadSceneMode mode)
+        //{
+        //    //Remove the callback
+        //    SceneManager.sceneLoaded -= OnPuzzleLoaded;
+
+        //    //Disable the main scene objects
+        //    foreach (GameObject obj in SceneManager.GetSceneByName(openScene).GetRootGameObjects())
+        //    {
+        //        if (!obj.active) { disabledObject.Add(obj); } else { obj.SetActive(false); }
+        //    }
+
+        //    //Enable the cursor
+        //    Cursor.lockState = CursorLockMode.None;
+        //    Cursor.visible = true;
+
+        //    //Set the scene as active
+        //    SceneManager.SetActiveScene(scene);
+        //}
+
+        //public void close(string name)
+        //{
+        //    //Add a callback for when the unloading completes
+        //    SceneManager.sceneUnloaded += OnPuzzleUnloaded;
+
+        //    //Load the scene
+        //    SceneManager.UnloadScene(name);
+        //}
+
+        //private void OnPuzzleUnloaded(Scene scene)
+        //{
+        //    //Remove the callback
+        //    SceneManager.sceneUnloaded -= OnPuzzleUnloaded;
+
+        //    //Enable the main scene objects
+        //    foreach (GameObject obj in SceneManager.GetSceneByName(openScene).GetRootGameObjects())
+        //    {
+        //        if (!disabledObject.Contains(obj)) { obj.SetActive(true); }
+        //    }
+
+        //    //Clear the list
+        //    disabledObject.Clear();
+
+        //    //Disable the cursor
+        //    Cursor.lockState = CursorLockMode.Locked;
+        //    Cursor.visible = false;
+
+        //    //Set the scene as active
+        //    SceneManager.SetActiveScene(SceneManager.GetSceneByName(openScene));
+        //}
+
         public void Update()
         {
-            if (LevelController.getInstance().CheckDoorCanOpen(puzzleNumber))
-            {
-                SetGlow(GlowColor.GREEN);
-            }
-            else
-            {
-                if (puzzleNumber == LevelController.getInstance().puzzlesCompleted.Count)
-                {
-                    SetGlow(GlowColor.RED);
-                }
-                else
-                {
-                    SetGlow(GlowColor.NONE);
-                }
-            }
+
         }
+        //public string puzzleName;
+
+        //public virtual void open()
+        //{
+        //    Assets.Source.Player.FirstPersonPlayer firstPersonPlayer = GameObject.FindObjectOfType<Assets.Source.Player.FirstPersonPlayer>();
+
+        //    Cursor.lockState = CursorLockMode.None;
+        //    Cursor.visible = true;
+
+        //    LevelController.getInstance().playerPosition = firstPersonPlayer.transform.position;
+
+        //                    SceneManager.LoadScene(puzzleName, LoadSceneMode.Single);
+
+        //}
 
     }
 }
